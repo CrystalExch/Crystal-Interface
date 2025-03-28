a2d058615576b43bef94398f089146a5d8e5ae2f
legionevm <167903176+legionevm@users.noreply.github.com>
2025-03-27 21:19:55 -0700

    s

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -81,8 +81,7 @@ const ChartComponent: React.FC<ChartComponentProps> = ({
     contractAddress: string
   ): Promise<DataPoint[]> {
     const seriesId = `series-${interval}-${contractAddress}`.toLowerCase();
-    // const endpoint = `https://gateway.thegraph.com/api/${settings.graphKey}/subgraphs/id/BDU1hP5UVEeYcvWME3eApDa24oBteAfmupPHktgSzu5r`;
-    const endpoint = 'https://api.studio.thegraph.com/query/104695/crystal-v2/v0.0.9';
+    const endpoint = `https://gateway.thegraph.com/api/${settings.graphKey}/subgraphs/id/6ikTAWa2krJSVCr4bSS9tv3i5nhyiELna3bE8cfgm8yn`;
 
     let allCandles: any[] = [];
     const query = `
