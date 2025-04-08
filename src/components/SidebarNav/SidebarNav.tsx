ae72bf78942ef87221230cbb8c7e5a3ae22f3f63
0xrubberbandz <174072155+0xrubberbandz@users.noreply.github.com>
2025-04-07 20:04:01 -0700

    'z

--- src/components/SidebarNav/SidebarNav.tsx
+++ src/components/SidebarNav/SidebarNav.tsx
@@ -3,7 +3,12 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
 import backgroundlesslogo from '../../assets/logo_clear.png';
 import './SidebarNav.css';
 import { useLanguage } from '../../contexts/LanguageContext';
-
+import mint from '../../assets/mint.png';
+import candlestick from '../../assets/candlestick.png';
+import portfolio from '../../assets/folder.png';
+import referrals from '../../assets/referrals.png';
+import leaderboard from '../../assets/leaderboard.png';
+import swap from '../../assets/swap.png';
 interface SidebarNavProps {
   simpleView: boolean;
   setSimpleView: (value: boolean) => void;
@@ -45,70 +50,46 @@ const SidebarNav: React.FC<SidebarNavProps> = ({ simpleView, setSimpleView }) =>
       <Link to="/" className="sidebar-logo">
         <img src={backgroundlesslogo} className="sidebar-logo-image" alt="Logo" />
       </Link>
+  
       
-      <div className="view-buttons-container">
+      <div className="sidebar-links">
+       <button 
+          className={`view-mode-button ${isTradingPage && !simpleView ? 'active' : ''}`}
+          onClick={goToAdvancedView}
+          title={t('advancedView')}
+        >
+          <img src={candlestick} alt="Advanced View" className="sidebar-icon" />
+          <span className="view-mode-text">{t('advanced')}</span>
+        </button>
         <button 
           className={`view-mode-button ${isTradingPage && simpleView ? 'active' : ''}`}
           onClick={goToSimpleView}
           title={t('basicView')}
         >
-          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-            <path d="M4 5h16M4 12h12M4 19h8" />
-          </svg>
-        </button>
-        
-        <button 
-          className={`view-mode-button ${isTradingPage && !simpleView ? 'active' : ''}`}
-          onClick={goToAdvancedView}
-          title={t('advancedView')}
-        >
-          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-            <line x1="6" y1="12" x2="6" y2="20"></line>
-            <line x1="6" y1="4" x2="6" y2="8"></line>
-            <line x1="12" y1="16" x2="12" y2="20"></line>
-            <line x1="12" y1="4" x2="12" y2="14"></line>
-            <line x1="18" y1="9" x2="18" y2="20"></line>
-            <line x1="18" y1="4" x2="18" y2="7"></line>
-            <line x1="4" y1="4" x2="8" y2="4"></line>
-            <line x1="4" y1="20" x2="8" y2="20"></line>
-            <line x1="10" y1="14" x2="14" y2="14"></line>
-            <line x1="10" y1="16" x2="14" y2="16"></line>
-            <line x1="16" y1="7" x2="20" y2="7"></line>
-            <line x1="16" y1="9" x2="20" y2="9"></line>
-          </svg>
-        </button>
-      </div>
-      
-      <div className="sidebar-links">
-      <Link to="/portfolio" className={path === '/portfolio' ? 'active' : ''}>
-          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-            <rect x="2" y="4" width="20" height="16" rx="2"></rect>
-            <path d="M16 10h4v4h-4a2 2 0 0 1 0-4z"></path>
-          </svg>
-        </Link>
-        <Link to="/referrals" className={path === '/referrals' ? 'active' : ''}>
-          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-            <circle cx="12" cy="12" r="10"></circle>
-            <line x1="2" y1="12" x2="22" y2="12"></line>
-            <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
-          </svg>
-        </Link>
-        
-        <Link to="/leaderboard" className={path === '/leaderboard' ? 'active' : ''}>
-          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-            <line x1="18" y1="20" x2="18" y2="10"></line>
-            <line x1="12" y1="20" x2="12" y2="4"></line>
-            <line x1="6" y1="20" x2="6" y2="14"></line>
-          </svg>
-        </Link>
-        
+          <img src={swap} alt="Basic View" className="sidebar-icon" />
+          <span className="view-mode-text">{t('simple')}</span>
 
+        </button>
+        <Link to="/portfolio" className={path === '/portfolio' ? 'active' : 'page-mode-button'}>
+          <img src={portfolio} alt="Portfolio" className="sidebar-icon" />
+          <span className="view-mode-text">{t('portfolio')}</span>
+
+        </Link>
+        <Link to="/referrals" className={path === '/referrals' ? 'active' : 'page-mode-button'}>
+          <img src={referrals} alt="Referrals" className="sidebar-icon" />
+          <span className="view-mode-text">{t('referrals')}</span>
+
+        </Link>
         
-        <Link to="/mint" className={path === '/mint' ? 'active' : ''}>
-          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-            <circle cx="12" cy="12" r="10"></circle>
-            <polyline points="12 6 12 12 16 14"></polyline>
-          </svg>
+        <Link to="/leaderboard" className={path === '/leaderboard' ? 'active' : 'page-mode-button'}>
+          <img src={leaderboard} alt="Leaderboard" className="sidebar-icon" />
+          <span className="view-mode-text">{t('leaderboard')}</span>
+
+        </Link>
+  
+        <Link to="/mint" className={path === '/mint' ? 'active' : 'page-mode-button'}>
+          <img className="sidebar-icon" src={mint}/>
+          <span className="view-mode-text">{t('mint')}</span>
         </Link>
       </div>
       
