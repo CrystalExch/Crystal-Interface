0dd08dc3002caeda69e30db5cfe60dbe4d8ddad1
0xrubberbandz <0xrubberbandz@proton.me>
2025-12-11 11:02:00 -0800

    z

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -3436,8 +3436,12 @@ const Portfolio: React.FC<PortfolioProps> = ({
                 </div>
 
               </div>
-
+                
               <div className="trenches-time-controls">
+                <div className="trenches-icon-buttons">
+                <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="meme-portfolio-search-icon"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="meme-portfolio-search-icon"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>
+                </div>
                 <div className="trenches-time-button">1d</div>
                 <div className="trenches-time-button">7d</div>
                 <div className="trenches-time-button">30d</div>
